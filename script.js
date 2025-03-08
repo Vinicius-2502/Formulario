5c35c2f6b5ccd98390fb1fbddf0a211249301cfb
Vinicius-2502 <vinicius.nascimento.250209@gmail.com>
2025-03-08 15:40:26 -0300

    Atualizando a pagina inicial e corrigindo as imagens

--- script.js
+++ script.js
@@ -72,22 +72,49 @@ const showInvite = (userData) => {
   </div>`
 }
 
-const updateImageLinks = () => {
-  document.querySelectorAll('img').forEach((imagem) => {
-    img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/${img.src}`
-  })
-}
-
 const startapp = () => {
-  const content = `<form id="form" >
-<input type="email" name="email" placeholder="email">
-<input type="text" name="phone" placeholder="telefone">
-<button>confirmar</button>
-</form> `
+  const content = `        <main>
+          <section class="about">
+            <div class="section-header">
+              <h2>Sobre o evento</h2>
+              <span class="badge">AO VIVO</span>
+            </div>
+            <p>
+              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento.Vamos
+              mergulhar nas tendencias mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, whorkshops e hackathons. <br /><br />
+              Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
+            </p>
+          </section>
+          <section class="registration">
+            <h2>Inscrição</h2>
+            <form id="form">
+              <div class="input-wrapper">
+                <div class="input-group">
+                  <label for="email"><img src="./imagens/mail.svg" alt="Email icon"></label>
+                  <input type="email" id="email" name="email" placeholder="E-mail">
+                </div>
+                
+                
+              <div class="input-wrapper">
+              <div class="input-group">
+              <label for="phone"><img src="./imagens/phone.svg" alt="Phone icon"></label>
+              <input type="text" id="phone" name="phone" placeholder="Telefone">
+              </div>
+
+              </div>
+
+                <button>Confirmar</button>
+                <img src="./imagens/arrow.svg" alt="arrow-right">
+            </form>
+          </section>
+        </main>
+`
   app.innerHTML = content
   updateImageLinks()
   formAction()
-  
 }
 startapp()
 
